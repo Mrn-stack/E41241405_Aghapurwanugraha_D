0423abf994a60496ade04f131408f4d70d1ea44c
Mrn-stack <aghapurwa2020@gmail.com>
2025-02-14 16:47:35 +0700

    Create tugas.java

---
+++ tugas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package minggu_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class tugas {
+
+    public static void main(String[] args) {
+        String fName = "Lisa Palembo";
+        String lName = "123456789";
+        String stuStudent = "Active";
+        
+        System.out.println(fName);
+        System.out.println(lName);
+        System.out.println(stuStudent);
+        
+                
+    }
+}
